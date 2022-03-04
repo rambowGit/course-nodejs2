776e4a1aaeb393f8c99ddc27e2da5b4909eff663
Stepkin Alexander <stepkin.alexander@gmail.com>
2022-03-04 22:33:26 +0300

    user form handler

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { products, skills } = require('../data.json')
 
+/*
+ middleware for web-form handling
+ used in router.post
+*/
+express().use(express.urlencoded({ extended: false }));
 
 router.get('/', (req, res, next) => {
   res.render('pages/index', { title: 'Main page', products, skills })
@@ -9,7 +14,8 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   
-
+  const userForm = req.body;
+  res.render('pages/userpage', {userForm});
 })
 
 module.exports = router
